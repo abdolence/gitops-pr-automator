31dc8c97dddd65aa9b407a08e52b3f7199690126
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-05-29 17:55:50 +0200

    PR summary formatting update

--- src/pull-request.ts
+++ src/pull-request.ts
@@ -301,10 +301,12 @@ async function generatePrSummaryText(
     prSummaryText += `### Versions:\n\n`
     prSummaryText += `\n\n :fast_forward: Updated to: [\`${repoChanges.currentVersion.slice(0, 9)}\`](https://github.com/${repoChanges.sourceRepo.repo}/commits/${repoChanges.currentVersion}).\n\nExisting versions:\n`
 
-    prSummaryText += `| Version | Files |\n`
+    prSummaryText += `| File | Current Version |\n`
     prSummaryText += `| ------- | ----- |\n`
     for (const version of repoChanges.repoVersionsToUpdate) {
-      prSummaryText += `| [\`${version.version.slice(0, 9)}\`](https://github.com/${repoChanges.sourceRepo.repo}/commits/${version.version}) | ${version.files.map(f => f.gitPath).join(', ')} |\n`
+      for (const file of version.files) {
+        prSummaryText += `| ${file.gitPath} | [\`${version.version.slice(0, 9)}\`](https://github.com/${repoChanges.sourceRepo.repo}/commits/${version.version}) |\n`
+      }
     }
 
     prSummaryText += `\n\n### :memo: Changes:\n`
